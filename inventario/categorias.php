6087b7170f2d3b1212c08b6ae346be1943c7dde2
Yuki xD <pxnditxyr@gmail.com>
2022-11-27 20:13:47 -0400

    feat: show table of categories

--- inventario/categorias.php
+++ inventario/categorias.php
@@ -1,17 +1,61 @@
 <?php 
   require_once './layouts/layout.php';
-function categorias () {
-  return '
-      <div>
-        <form action="actions/crearCategoria.php" method="POST">
-          <input type="text" name="nombre" placeholder="Nombre">
-          <input type="text" name="detalles" placeholder="Detalles">
-          <input type="text" name="descripcion" placeholder="Descripcion">
-          <input type="submit" name="crear" value="Crear">
-        </form>
-      </div>
-    ';
+  require_once '../models/Categoria.php';
+
+  function crearTabla = ( $categorias ) {
+    $tabla = '';
+    foreach ( $categorias as $categoria ) {
+      $tabla += '<tr>';
+      $tabla += '<td>' . $categoria->id . '</td>';
+      $tabla += '<td>' . $categoria->nombre . '</td>';
+      $tabla += '<td>' . $categoria->descripcion . '</td>';
+      $tabla += '<td>' . $categoria->estado . '</td>';
+      $tabla += '<td>';
+      $tabla += '<a href="editar.php?id=' . $categoria->id . '" class="btn btn-warning">Editar</a>';
+      $tabla += '<a href="eliminar.php?id=' . $categoria->id . '" class="btn btn-danger">Eliminar</a>';
+      $tabla += '</td>';
+      $tabla += '</tr>';
+    }
+    return $tabla;
   }
+
+  function categorias () {
+    $categoria = new Categoria();
+    $categoriasObtenidas = $categoria -> obtenerCategorias();
+    $tabla = crearTabla( $categoriasObtenidas );
+
+    
+    return '
+        <div>
+          <form action="actions/crearCategoria.php" method="POST">
+            <input type="text" name="nombre" placeholder="Nombre">
+            <input type="text" name="detalles" placeholder="Detalles">
+            <input type="text" name="descripcion" placeholder="Descripcion">
+            <input type="submit" name="crear" value="Crear">
+          </form>
+        </div>
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th>Nombre</th>
+                <th>Detalles</th>
+                <th>Descripcion</th>
+                <th>Acciones</th>
+              </tr>
+            </thead>
+            <tbody>
+              ' . $tabla . '
+            </tbody>
+          </table>
+        </div>
+
+      ';
+  }
+  
   $categorias = categorias();
+  $categoriasObtenidas = 
   echo layout( 'Categorias', $categorias );
 ?>
+
+
