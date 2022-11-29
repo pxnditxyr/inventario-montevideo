7bacc9f616c2ebe84a16964b69b77aeda27dbf3d
Yuki xD <pxnditxyr@gmail.com>
2022-11-29 16:53:10 -0400

    feat: select for categoria

--- inventario/editarProducto.php
+++ inventario/editarProducto.php
@@ -1,5 +1,6 @@
 <?php
   include_once 'layouts/layout.php';
+  include_once '../models/Categoria.php';
   if ( !isset( $_GET[ 'id' ] ) ) {
     echo '<h1> Ha ocurrido un error </h1>';
     header( 'Refresh: 3; URL=productos.php' );
@@ -21,19 +22,35 @@
   function crearFormulario ( $producto ) {
     $inputs = '';
     foreach ( $producto as $key => $value ) {
-      if ( strcmp( $key, 'id' ) != 0 && strcmp( $key, 'estado' ) != 0 && strcmp( $key, 'created_at' ) != 0 && strcmp( $key, 'updated_at' ) != 0 ) {
-        // if type of value is date input type is date with regex
-        $regexIsDate = preg_match( '/\d{4}-\d{2}-\d{2}/', $value );
-        if ( $regexIsDate ) {
-          $inputs .= '<div class="form-group">
-                        <label for="' . $key . '">' . ucfirst( $key ) . '</label>
-                        <input type="date" class="form-control" id="' . $key . '" name="' . $key . '" value="' . $value . '" required>
-                      </div>';
-        } else {
-          $inputs .= '<div class="form-group">
-                        <label for="' . $key . '">' . ucfirst( $key ) . '</label>
-                        <input type="text" class="form-control" id="' . $key . '" name="' . $key . '" value="' . $value . '" required>
-                      </div>';
+      if ( strcmp( $key, 'categoria' ) == 0 ) {
+        $categoria = new Categoria();
+        $categorias = $categoria -> obtenerCategorias();
+        $options = '';
+        foreach ( $categorias as $categoria ) {
+          $options .= '<option value="' . $categoria[ 'id' ] . '">' . $categoria[ 'nombre' ] . '</option>';
+        }
+        $inputs .= '<div class="form-group">
+                      <label for="categoria">Categoria</label>
+                      <select class="form-control" name="categoria" id="categoria">
+                        ' . $options . '
+                      </select>
+                    </div>';
+
+      } else {
+
+        if ( strcmp( $key, 'id' ) != 0 && strcmp( $key, 'estado' ) != 0 && strcmp( $key, 'created_at' ) != 0 && strcmp( $key, 'updated_at' ) != 0 ) {
+          $regexIsDate = preg_match( '/\d{4}-\d{2}-\d{2}/', $value );
+          if ( $regexIsDate ) {
+            $inputs .= '<div class="form-group">
+                          <label for="' . $key . '">' . ucfirst( $key ) . '</label>
+                          <input type="date" class="form-control" id="' . $key . '" name="' . $key . '" value="' . $value . '" required>
+                        </div>';
+          } else {
+            $inputs .= '<div class="form-group">
+                          <label for="' . $key . '">' . ucfirst( $key ) . '</label>
+                          <input type="text" class="form-control" id="' . $key . '" name="' . $key . '" value="' . $value . '" required>
+                        </div>';
+          }
         }
       }
     }
