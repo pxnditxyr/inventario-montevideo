f6fa1ef3f9331567bea960c23110e5b0c0f63858
Yuki xD <pxnditxyr@gmail.com>
2022-11-30 17:13:10 -0400

    style: part of register

--- register.php
+++ register.php
@@ -3,15 +3,35 @@
   
   function register () {
     return '
-<form action="actions/register.php" method="post">
-  <div>
-    <label for="apellidos"> Apellidos: </label>
-    <input type="text" name="apellidos" id="apellidos" placeholder="Lima" value="Lima"/>
-  </div>
-  <div>
-    <label for="nombres"> Nombres: </label>
-    <input type="text" name="nombres" id="nombres" placeholder="Dennis" value="Dennis"/>
-  </div>
+
+<div style="background-image: url( assets/images/auth-image-2.jpg )" class="flex items-center h-full w-full bg-cover bg-center">
+      <div class="w-1/3 h-full flex flex-col-reverse p-10 gap-5" style="background: rgba(0,0,0, 0.5);">
+        <span class="text-sm text-white"> Copyright @ 2022 </span>
+        <p class="text-3xl font-bold text-white">
+          Tienda Jannet S.R.L.
+        </p>
+      </div>
+      <div class="w-1/3 h-full" style="background: rgba(0,0,0, 0.5);"></div>
+      <div class="w-1/3 h-full">
+
+    <form action="actions/register.php" method="post"
+      class="flex flex-col items-center justify-center w-full h-full bg-white shadow-lg p-6">
+    >
+      <div class="flex flex-col items-center justify-center w-full p-4 bg-white rounded-lg shadow-lg">
+        <h1 class="text-4xl font-bold text-slate-700"> Sistema de Inventario </h1>
+        <p class="text-xl font-bold text-slate-600 justify-start"> Inicio de sesion </p>
+      </div>
+
+    <div
+      class="flex flex-col justify-start w-full bg-white rounded-lg shadow-lg gap-3"
+    >
+      <label for="apellidos"> Apellidos: </label>
+      <input type="text" name="apellidos" id="apellidos" placeholder="Lima" value="Lima"/>
+    </div>
+    <div>
+      <label for="nombres"> Nombres: </label>
+      <input type="text" name="nombres" id="nombres" placeholder="Dennis" value="Dennis"/>
+    </div>
   <div>
     <label for="ci"> C.I.: </label>
     <input type="number" name="ci" id="ci" placeholder="12345678" value="12345678"/>
