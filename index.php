0b9504a78df493d044185c8ef20f7ff476dc3c68
lima <lima@lima.com>
2022-11-30 16:11:59 -0400

    style: fix navbar

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
-<?php 
+<?php
   require_once './layouts/layout.php';
+  function home() {
+    return '
+      <div>
+
+      </div>
+    ';
+  }
   echo layout( 'Home', 'Welcome to the Home' );
 ?>
--- views/navbar.php
+++ views/navbar.php
@@ -1,12 +1,12 @@
 <?php
   function navbar () {
     return '
-    <nav class="flex items-center justify-between flex-wrap bg-teal-500 p-6">
-      <ul class="flex items-center flex-shrink-0 text-white gap-20">
-        <li class="text-sm font-bold tracking-tight"><a href="index.php"> Home </a></li>
-        <li class="text-sm font-bold tracking-tight"><a href="about.php"> About </a></li>
-        <li class="text-sm font-bold tracking-tight"><a href="login.php"> Iniciar Sesion </a></li>
-        <li class="text-sm font-bold tracking-tight"><a href="register.php"> Registro </a></li>
+    <nav class="flex items-center justify-between flex-wrap bg-red-500 w-full justify-center">
+      <ul class="flex items-center flex-shrink-0 text-white gap-20 w-full justify-center">
+        <li><a href="index.php"><div class="text-xl font-bold p-6"> Home </div></a></li>
+        <li><a href="about.php"><div class="text-xl font-bold p-6"> About </div></a></li>
+        <li><a href="login.php"><div class="text-xl font-bold p-6"> Iniciar Sesion </div></a></li>
+        <li><a href="register.php"><div class="text-xl font-bold p-6"> Registro </div></a></li>
       </ul>
     </nav>';
   }
