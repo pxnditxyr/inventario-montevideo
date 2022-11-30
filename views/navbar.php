e80cc0f428063b67fa59df8cb6a4948a4c56d087
Yuki xD <pxnditxyr@gmail.com>
2022-11-30 16:00:10 -0400

    style: navbar

--- views/navbar.php
+++ views/navbar.php
@@ -1,13 +1,13 @@
 <?php
   function navbar () {
     return '
-<nav>
-  <ul>
-    <li><a href="index.php"> Home </a></li>
-    <li><a href="about.php"> About </a></li>
-    <li><a href="login.php"> Iniciar Sesion </a></li>
-    <li><a href="register.php"> Registro </a></li>
-  </ul>
-</nav>';
+    <nav class="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+      <ul class="flex items-center flex-shrink-0 text-white gap-20">
+        <li class="text-sm font-bold tracking-tight"><a href="index.php"> Home </a></li>
+        <li class="text-sm font-bold tracking-tight"><a href="about.php"> About </a></li>
+        <li class="text-sm font-bold tracking-tight"><a href="login.php"> Iniciar Sesion </a></li>
+        <li class="text-sm font-bold tracking-tight"><a href="register.php"> Registro </a></li>
+      </ul>
+    </nav>';
   }
 ?>
