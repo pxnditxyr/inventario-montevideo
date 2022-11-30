7bb4ce758f5a5c88fb8ece9c0ff801c4dc72d528
Yuki xD <pxnditxyr@gmail.com>
2022-11-30 17:59:24 -0400

    style: table categoria

--- inventario/categorias.php
+++ inventario/categorias.php
@@ -8,14 +8,14 @@
     foreach ( $categorias as $categoria ) {
       $tabla .= '<tr>';
       // $tabla .= '<td>' . $categoria[ 'id' ] . '</td>';
-      $tabla .= '<td>' . $i . '</td>';
-      $tabla .= '<td>' . $categoria[ 'nombre' ] . '</td>';
-      $tabla .= '<td>' . $categoria[ 'detalles' ] . '</td>';
-      $tabla .= '<td>' . $categoria[ 'descripcion' ] . '</td>';
+      $tabla .= '<td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">' . $i . '</td>';
+      $tabla .= '<td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">' . $categoria[ 'nombre' ] . '</td>';
+      $tabla .= '<td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">' . $categoria[ 'detalles' ] . '</td>';
+      $tabla .= '<td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">' . $categoria[ 'descripcion' ] . '</td>';
       //$tabla .= '<td>' . $categoria[ 'estado' ] . '</td>';
-      $tabla .= '<td>';
-      $tabla .= '<a href="editarCategoria.php?id=' . $categoria[ 'id' ] . '" class="btn btn-warning">Editar</a>';
-      $tabla .= '<a href="actions/eliminarCategoria.php?id=' . $categoria[ 'id' ] . '" class="btn btn-danger">Eliminar</a>';
+      $tabla .= '<td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">';
+      $tabla .= '<a href="editarCategoria.php?id=' . $categoria[ 'id' ] . '" class="text-indigo-600 hover:text-indigo-900">Editar</a>';
+      $tabla .= '<a href="actions/eliminarCategoria.php?id=' . $categoria[ 'id' ] . '" class="text-indigo-600 hover:text-indigo-900 ml-4">Eliminar</a>';
       $tabla .= '</td>';
       $tabla .= '</tr>';
       $i++;
@@ -30,24 +30,24 @@
 
 
     return '
-        <div>
-          <form action="actions/crearCategoria.php" method="POST">
-            <input type="text" name="nombre" placeholder="Nombre">
-            <input type="text" name="detalles" placeholder="Detalles">
-            <input type="text" name="descripcion" placeholder="Descripcion">
-            <input type="submit" name="crear" value="Crear">
+        <div class="flex flex-col p-5">
+          <form action="actions/crearCategoria.php" method="POST" class="flex flex-col gap-4">
+            <input class="border border-gray-300 p-2 rounded-md" type="text" name="nombre" placeholder="Nombre">
+            <input class="border border-gray-300 p-2 rounded-md" type="text" name="detalles" placeholder="Detalles">
+            <input class="border border-gray-300 p-2 rounded-md" type="text" name="descripcion" placeholder="Descripcion">
+            <input class="border border-gray-300 p-2 rounded-md" type="submit" name="crear" value="Crear">
           </form>
         </div>
-        <div>
-          <table>
-            <thead>
+        <div class="flex flex-col p-5">
+          <table class="min-w-full divide-y divide-gray-200">
+            <thead class="border-b">
               <tr>
-                <th>Numero</th>
-                <th>Nombre</th>
-                <th>Detalles</th>
-                <th>Descripcion</th>
+                <th class="text-sm font-medium text-gray-900 px-6 py-4 text-left">Numero</th>
+                <th class="text-sm font-medium text-gray-900 px-6 py-4 text-left">Nombre</th>
+                <th class="text-sm font-medium text-gray-900 px-6 py-4 text-left">Detalles</th>
+                <th class="text-sm font-medium text-gray-900 px-6 py-4 text-left">Descripcion</th>
                 <!-- <th>Estado</th> -->
-                <th>Acciones</th>
+                <th class="text-sm font-medium text-gray-900 px-6 py-4 text-left">Acciones</th>
               </tr>
             </thead>
             <tbody>
