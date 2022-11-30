e0f8b09e2a7e7d498117c2936a8bc01e0ccfce76
Yuki xD <pxnditxyr@gmail.com>
2022-11-30 16:25:55 -0400

    style: login 2

--- login.php
+++ login.php
@@ -4,17 +4,17 @@
   function login () {
     return '
     <div style="background-image: url( assets/images/auth-image-1.jpg )" class="flex flex-col items-center justify-center h-full w-full bg-cover bg-center">
-      <form action="actions/login.php" method="post" class="flex flex-col items-center justify-center w-1/2">
-        <div>
-            <label for="email"> Email: </label>
-            <input type="email" name="email" id="email" placeholder="example@example.com" value="dennis@dennis.com"/>
+      <form action="actions/login.php" method="post" class="flex flex-col items-center justify-center w-1/2 bg-white rounded-lg shadow-lg">
+        <div class="flex flex-col items-center justify-center w-full p-4 bg-white rounded-lg shadow-lg">
+          <label for="email" class="text-gray-700 text-lg font-semibold mb-2" > Email: </label>
+          <input class="w-full p-2 border border-gray-300 rounded-lg mb-4" type="email" name="email" id="email" placeholder="example@example.com" value="dennis@dennis.com"/>
         </div>
-        <div>
-            <label for="password"> Contraseña: </label>
-            <input type="password" name="password" id="password" placeholder="" value="12345" />
+        <div class="flex flex-col items-center justify-center w-full p-4 bg-white rounded-lg shadow-lg" >
+          <label for="password" class="text-gray-700 text-lg font-semibold mb-2" > Contraseña: </label>
+          <input class="w-full p-2 border border-gray-300 rounded-lg mb-4" type="password" name="password" id="password" placeholder="" value="12345" />
         </div>
-        <div>
-            <button> Ingresar </button>
+        <div class="flex flex-col items-center justify-center w-full p-4 bg-white rounded-lg shadow-lg">
+          <button type="submit" class="w-full p-2 bg-blue-500 text-white rounded-lg shadow-lg">Iniciar sesión</button>
         </div>
       </form>
     </div>
